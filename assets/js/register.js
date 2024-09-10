42e20f0692add6f6f9591667cc0bc001e4877358
Belalwesam <belal.ahmaf@gmail.com>
2024-09-10 18:40:49 +0300

    the active state for the inputs of type radio added

--- assets/js/register.js
+++ assets/js/register.js
@@ -3,9 +3,12 @@ let radios = document.querySelectorAll('.register-radio')
 radios.forEach(radio => {
     radio.addEventListener("change", (radio) => {
         radios.forEach(radio => {
+            radio.previousElementSibling.querySelectorAll('img')[0].classList.remove('d-none')
+            radio.previousElementSibling.querySelectorAll('img')[1].classList.add('d-none')
             if (radio.checked) {
-                console.log(radio);
                 radio.previousElementSibling.classList.add('active')
+                radio.previousElementSibling.querySelectorAll('img')[0].classList.add('d-none')
+                radio.previousElementSibling.querySelectorAll('img')[1].classList.remove('d-none')
             } else {
                 radio.previousElementSibling.classList.remove('active')
             }
