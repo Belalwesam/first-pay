669316bc2f9938b48a76dc697b15437cfb85d992
Belalwesam <belal.ahmaf@gmail.com>
2024-09-10 17:10:47 +0300

    the active status for buttons added

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,7 +1,14 @@
 let radios = document.querySelectorAll('.register-radio')
 
 radios.forEach(radio => {
-    radio.addEventListener("change", () => {
-        console.log('world');
+    radio.addEventListener("change", (radio) => {
+        radios.forEach(radio => {
+            if (radio.checked) {
+                console.log(radio);
+                radio.previousElementSibling.classList.add('active')
+            } else {
+                radio.previousElementSibling.classList.remove('active')
+            }
+        });
     })
 })
